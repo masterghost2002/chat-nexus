49c93b5a777028d0dc7ae15a9f44f5bc26992194
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-03-03 21:23:09 +0530

    index.ts fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-import Redis from './providers/Redis';
-const getRedisInstance = Redis.getRedisInstance(6379, 'localhost');
-getRedisInstance.setKey('name', 'John Doe');
+export * from './providers/ChatNexus';
